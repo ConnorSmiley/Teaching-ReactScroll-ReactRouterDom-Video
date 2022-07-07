230ad86f6e719138541cee55dd6582d43ac5e4e6
Connor Smiley <60119998+ConnorSmiley@users.noreply.github.com>
2022-07-07 22:45:35 +0900

    Update Main.tsx

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import GlobalStyles from './styles/GlobalStyles'
 import App from './App'
-import store from './Redux/store'
+import {store} from './Redux/store'
 import { Provider } from 'react-redux'
 
 
